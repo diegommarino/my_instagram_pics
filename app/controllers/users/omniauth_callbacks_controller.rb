3ba3a0609b0af093e67af800df64b429f936b998
diegommarino <diegommarino@gmail.com>
2018-02-11 01:06:20 +0000

    Adding style and fixing login
    - Added some css and fonts to give the app a Instagram feel.
    - Fixed the token renovation problem when a new token is generated.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,15 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def instagram
     @user = User.from_omniauth(request.env["omniauth.auth"])
-
-    #if @user.persisted?
+    @user.token = request.env["omniauth.auth"].credentials.token
+    @user.save
+    if @user.persisted?
       session[:instagram_token] = request.env["omniauth.auth"].credentials.token
       session["devise.instagram_data"] = request.env["omniauth.auth"]
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "Instagram") if is_navigational_format?
-    #else
-      #redirect_to root_path
-    #end
+    else
+      set_flash_message(:notice, :failure, kind: "Instagram") if is_navigational_format?
+      redirect_to root_path
+    end
   end
 
   def failure
